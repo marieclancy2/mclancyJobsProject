c9e10efdf085cb06a375fb37fed3c85c3fd401ee
Marie Clancy <marie.clancy412@gmail.com>
2020-02-10 12:03:16 -0500

    push with new tests and database feature

--- Tests/testJobs.py
+++ Tests/testJobs.py
@@ -5,6 +5,19 @@ import marieclancyProject1
 def test_get_jobs():
     jobs = marieclancyProject1.get_jobs()
     assert len(jobs) > 100
+    assert type(jobs[1]) == dict
+
+
+def test_insert_to_database():
+    jobs = marieclancyProject1.get_jobs()
+    existingTitle = "Web Full Stack Engineer"
+    conn, cursor = marieclancyProject1.open_db("test.sqlite")
+    marieclancyProject1.setup_db(cursor, conn)
+    for job in jobs:
+        marieclancyProject1.insert_to_database(cursor, conn, job)
+    cursor.execute("SELECT * FROM jobs WHERE jobs.title = ?", (existingTitle,))
+    assert cursor.fetchone()
+    marieclancyProject1.close_db(conn)
 
 
 # Test to check if the function actually writes a file with the correct data.
@@ -19,3 +32,33 @@ def test_write_file():
                 match = True
                 break
     assert match
+
+
+def test_send_extra_data():
+    conn, cursor = marieclancyProject1.open_db("test.sqlite")
+    marieclancyProject1.setup_db(cursor, conn)
+    extraGoodData = {
+        "id": "781", "type": "yes", "url": "ok.com", 'company': 'google',
+        'company_url': 'ok.com123',
+        'created_at': "March 1, 2019", 'location': "USA", 'title': 'senior designer',
+        'description': "professional developer needed",
+        'how_to_apply': "please visit website", 'company_logo': "none"}
+    marieclancyProject1.insert_to_database(cursor, conn, extraGoodData)
+    existingID = "781"
+    cursor.execute("SELECT * FROM jobs WHERE jobs.id = ?", (existingID,))
+    assert cursor.fetchone()
+
+    # same as above but with fewer arguments
+    extraBadData = {
+        "id": "782", "type": "yes", "url": "ok.com", 'company': 'google',
+        'title': 'senior designer',
+        'description': "professional developer needed",
+        'how_to_apply': "please visit website", 'company_logo': "none"}
+
+    nonExistingID = 782
+    marieclancyProject1.insert_to_database(cursor, conn, extraBadData)
+    cursor.execute("SELECT * FROM jobs WHERE jobs.id = ?", (nonExistingID,))
+    assert cursor.fetchone() is None
+
+    marieclancyProject1.close_db(conn)
+
--- marieclancyProject1.py
+++ marieclancyProject1.py
@@ -1,22 +1,92 @@
 import requests
 import time
 import json
+import sqlite3
+from typing import Tuple
+
+# function to open a database
+def open_db(filename:str)->Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    db_connection = sqlite3.connect(filename) # connect to existing DB or create new one
+    cursor = db_connection.cursor() # get ready to read/write data
+    return db_connection, cursor
+
+
+# Function to close a database
+def close_db(connection:sqlite3.Connection):
+    connection.commit() # make sure any changes get saved
+    connection.close()
+
+
+# Function to commit to database
+def commit_db(connection:sqlite3.Connection):
+    connection.commit()
 
 
 # Main function that retrieves the jobs and writes them to a file.
 def main():
     jobs = get_jobs()
     write_file(jobs)
+    conn, cursor = open_db("jobs.sqlite")
+    setup_db(cursor, conn)
+    conn.commit()
+    for job in jobs:
+        insert_to_database(cursor, conn, job)
+    close_db(conn)
 
 
-# Function that retrives the jobs.
+# Function that creates the table jobs
+def setup_db(cursor:sqlite3.Cursor, connection:sqlite3.Connection):
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS jobs( 
+    id TEXT PRIMARY KEY,
+    type TEXT NOT NULL,
+    url TEXT NOT NULL,
+    company TEXT NOT NULL,
+    company_url TEXT,
+    created_at TEXT,
+    location TEXT,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    how_to_apply TEXT,
+    company_logo TEXT
+    );''')
+    connection.commit()
+
+
+def insert_to_database(cursor:sqlite3.Cursor, connection:sqlite3.Connection,  data: dict):
+    if len(data) != 11:
+        return
+    try:
+        cursor.execute(f'''
+        INSERT INTO jobs (id, type, url, company, company_url, created_at, location, title, description,
+        how_to_apply, company_logo)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''', (data['id'], data['type'], data['url'], data['company'], data['company_url'],
+                                                  data['created_at'], data['location'], data['title'], data['description'],
+                                                  data['how_to_apply'], data['company_logo'],))
+    except sqlite3.IntegrityError:
+        pass
+
+    connection.commit()
+
+
+# Function that retrieves the jobs.
 def get_jobs():
-    url = "https://jobs.github.com/positions.json?page="
-    totalJSON = []
-    for counter in range(1, 6):
-        totalJSON += requests.get(url + str(counter)).json()
-        time.sleep(2)
-    return totalJSON
+    """retrieve github jobs data in form of a list of dictionaries after json processing"""
+    all_data = []
+    page = 1
+    more_data = True
+    while more_data:
+        url = f"https://jobs.github.com/positions.json?page={page}"
+        raw_data = requests.get(url)
+        if "GitHubber!" in raw_data:  # sometimes if I ask for pages too quickly I get an error; only happens in testing
+            continue  # trying continue, but might want break
+        partial_jobs_list = raw_data.json()
+        all_data.extend(partial_jobs_list)
+        if len(partial_jobs_list) < 50:
+            more_data = False
+        time.sleep(.1)  # short sleep between requests so I dont wear out my welcome.
+        page += 1
+    return all_data
 
 
 # Function that writes the data to a file.
